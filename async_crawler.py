63e87230124104cf4a7b76536bb63885c0ebb602
Adrian Prach <263222609+adrianprach@users.noreply.github.com>
2026-04-08 07:54:59 +0930

    feat: add max concurrency and max pages

--- async_crawler.py
+++ async_crawler.py
@@ -1,4 +1,5 @@
 import asyncio
+from urllib.parse import urlparse
 import aiohttp
 
 from crawl import extract_page_data, normalize_url
@@ -7,15 +8,17 @@ from crawl import extract_page_data, normalize_url
 class AsyncCrawler:
     def __init__(self, base_url, max_concurrency, max_pages):
         self.base_url = base_url
-        self.base_domain = normalize_url(base_url)
+        self.base_domain = urlparse(base_url).netloc
         self.page_data = dict()
         self.lock = asyncio.Lock()
         self.semaphore = asyncio.Semaphore(max_concurrency)
-        self.max_pages = max_pages
         self.session = None
+        self.max_pages = max_pages
+        self.should_stop = False
+        self.all_tasks = set()
 
     async def __aenter__(self):
-        self.session = aiohttp.ClientSession(base_url=self.base_url)
+        self.session = aiohttp.ClientSession()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
@@ -24,6 +27,14 @@ class AsyncCrawler:
 
     async def add_page_visit(self, normalized_url):
         async with self.lock:
+            if self.should_stop:
+                return False
+            if len(self.page_data.keys()) >= self.max_pages:
+                self.should_stop = True
+                print(f"Reached maximum number of pages to crawl. {self.page_data.keys()}")
+                for task in list(self.all_tasks):
+                    task.cancel()
+                return False
             if normalized_url in self.page_data:
                 return False
             return True
@@ -52,30 +63,45 @@ class AsyncCrawler:
             return ""
 
     async def crawl_page(self, current_url=None):
-        base_url = self.base_url
-        page_data = self.page_data
+        if self.should_stop:
+            print("should stop", current_url)
+            return self.page_data
 
+        base_url = self.base_url
         if current_url is None:
-            return self.crawl_page(base_url, base_url, page_data)
-        if page_data is None:
-            page_data = dict()
+            current_url = base_url
 
         url_normalized = normalize_url(current_url)
         same_domain = normalize_url(base_url) in url_normalized
 
         first_time = await self.add_page_visit(url_normalized)
         if not first_time or (not same_domain):
-            return page_data
+            # print(f"early return {self.page_data.keys()}")
+            return self.page_data
 
-        tasks = []
+        tasks = set()
         async with self.semaphore:
+            print(f"crawling: {current_url}")
             page_html = await self.get_html_or_empty(current_url)
             result = extract_page_data(page_html, current_url)
+
             async with self.lock:
                 self.page_data[url_normalized] = result
+
             for link in result["outgoing_links"]:
-                tasks.append(asyncio.create_task(self.crawl_page(link)))
-        await asyncio.gather(*tasks)
+                task = asyncio.create_task(self.crawl_page(link))
+                try:
+                    task.add_done_callback(self.all_tasks.discard)
+                    tasks.add(task)
+                    self.all_tasks.add(task)
+                except Exception as e:
+                    print(f"error encountered when adding or callback for task {task}, {e}")
+                
+        try:
+            await asyncio.gather(*tasks, return_exceptions=True)
+        except Exception as e: 
+            print(f"error enocunted when running tasks: {e}")
+            
 
     async def crawl(self):
         await self.crawl_page(self.base_url)
--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import sys
 import asyncio
 import time
+import json
 
 from async_crawler import AsyncCrawler
 
 
-async def crawl_site_async(url):
-    async with AsyncCrawler(url, 20, 2) as site:
+async def crawl_site_async(url, max_concurrency, max_pages):
+    async with AsyncCrawler(url, max_concurrency, max_pages) as site:
         return await site.crawl()
 
 async def main():
+    max_concurrency = 5
+    max_pages = 5
     argv_len = len(sys.argv)
     if argv_len < 2:
         print("no website provided")
         return sys.exit(1)
-    if argv_len > 2:
+    if argv_len > 4:
         print("too many arguments provided")
         return sys.exit(1)
+    if argv_len == 4:
+        max_concurrency = sys.argv[2]
+        max_pages = sys.argv[3]
+
     base_url = sys.argv[1]
-    print(f"starting crawl of {base_url}")
+    print(f"starting crawl of {base_url}, max_concurrency: {max_concurrency}, max_pages: {max_pages}")
     start = time.perf_counter()
-    crawled = await crawl_site_async(base_url)
+    crawled = await crawl_site_async(base_url, int(max_concurrency), int(max_pages))
     elapsed = time.perf_counter() - start
     print(crawled)
+    print(json.dumps(obj=crawled, indent=2), f"Page crawled: {crawled.keys()}")
+    print(f"\n\nPage crawled: {crawled.keys()}")
     print(f"Elapsed: {elapsed:6f}ms")
     return sys.exit(0)
 
